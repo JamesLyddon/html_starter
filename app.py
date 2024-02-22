214646b44a2ec79e35b063ddd5bfba9998b730c2
JamesLyddon <jameslyddon@gmail.com>
2024-02-22 17:08:09 +0000

    added album and artist files

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
-
+from lib.album import Album
+from lib.artist import Artist
+from lib.album_repository import AlbumRepository
+from lib.artist_repository import ArtistRepository
 # Create a new Flask app
 app = Flask(__name__)
 
@@ -9,6 +12,55 @@ app = Flask(__name__)
 @app.route('/goodbye')
 def get_goodbye():
     return render_template('goodbye.html', farewell='Bye!')
+
+
+# == Artist / Album Routes ==
+@app.route('/artists')
+def get_artists():
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    return "\n".join(
+        f"{artist}" for artist in repository.all()
+    )
+
+@app.route('/artists', methods=["POST"])
+def post_artists():
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    artist = Artist(
+        None,
+        request.form["name"],
+        request.form["genre"],
+    )
+    repository.create(artist)
+    return '', 200
+
+@app.route('/albums')
+def get_albums():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    return "\n".join(
+        f"{album}" for album in repository.all()
+    )
+
+@app.route('/albums', methods=['POST'])
+def post_albums():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    album = Album(
+        None,
+        request.form['title'],
+        request.form['release_year'],
+        request.form['artist_id'],
+    )
+    repository.create(album)
+    return '', 200
+
+
+
+
+
+
 
 # == Example Code Below ==
 
